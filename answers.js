0b43ae93a8973095a7fa506d12a46a6b47f2b09e
Alexander Rowland <arowland900@gmail.com>
2019-06-07 09:11:28 -0700

    add day 2 refactor

--- answers.js
+++ answers.js
@@ -5,3 +5,18 @@ function factorial(n) {
     }
     return n * factorial(n - 1)
 }
+// DAY 2
+function gcd(n1, n2, i = Math.ceil(n1 > n2 ? n1 : n2)) {
+    if (n1 % i == 0 && n2 % i == 0) {
+        return i
+    }
+    return gcd(n1, n2, i - 1)
+}
+
+// DAY 2 Refactor:
+function gcd(n1, n2) {
+    if (!n2) {
+        return n1
+    }
+    return gcd(n2, n1 % n2)
+}
